6183d4d2eddcf7f06ed1fd58de573a3f3990e0d5
Isis Moran <imoran@mail.ccsf.edu>
2020-09-22 22:05:27 -0700

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
         const response = await unsplash.get('https://api.unsplash.com/search/photos', {
             params: { query: term }
         });
-        console.log(response.data.results)
         this.setState({ images: response.data.results });
     }
 
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './imageList.css';
 
 const ImageList = (props) => {
-    const images = props.images;
-    return (
-        <h1>
-            {images.map(({ description, id, urls }) => {
-               return <img alt={description} key={id} src={urls.regular} />
-            })}
-        </h1>
-    );
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
+    });
+
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
